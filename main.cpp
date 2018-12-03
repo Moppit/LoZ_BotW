54afa02eb52d46f4852bd9cca7203445ad52ccc3
Michelle Tran <michelletranusa@gmail.com>
2018-12-03 13:19:25 -0700

    Script to run the game in Repl.it

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Script to run the game in Repl.it: https://repl.it/repls/IcyJoyousTypes
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+  system("chmod 777 a.out");
+  system("./a.out");
+}
